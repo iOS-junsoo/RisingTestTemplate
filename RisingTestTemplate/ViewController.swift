bc7fc61988ad832dce9e29044339877256ab5060
kimjunsoo <rlawnstn01023@naver.com>
2021-10-17 20:24:22 +0900

    Initial Commit

---
+++ RisingTestTemplate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RisingTestTemplate
+//
+//  Created by 준수김 on 2021/10/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
